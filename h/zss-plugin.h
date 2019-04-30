fa5ea9580ba03db711b02020714839e5fd5b2593
Fyodor Kovin <fkovin@rocketsoftware.com>
2019-04-30 10:12:08 -0400

    Side deck for plugins
    - side deck that a plugin can use to link with the functions in the server
    - script that I used to generate it
    - header file that has all the functions that are in the side deck
    
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

---
+++ h/zss-plugin.h
@@ -0,0 +1,12 @@
+#ifndef __ZSS_PLUGIN_H__
+#define __ZSS_PLUGIN_H__
+
+#include "zowetypes.h"
+#include "httpserver.h"
+#include "utils.h"
+#include "dataservice.h"
+#include "json.h"
+#include "alloc.h"
+#include "logging.h"
+
+#endif /* __ZSS_PLUGIN_H__ */
