891d207f6d251145336862d36bdc93e989b94e65
Fyodor Kovin <fkovin@rocketsoftware.com>
2019-05-06 10:12:26 -0400

    add line breaks
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

--- build/gen-side-deck.sh
+++ build/gen-side-deck.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+trap 'exit 1' ERR
+
 FILES="recovery.o http.o httpserver.o utils.o dataservice.o json.o alloc.o logging.o"
 
 cd tmp-zss
 dd if=zssServer.x  conv=unblock cbs=80  of=side-deck
 nm $FILES |grep  ' T ' |awk '{ print $3 }' |sort -u >relevant-functions
-cat relevant-functions |while read fn; do grep   ",'"$fn"'" side-deck; done > side-deck-filtered
+cat relevant-functions |while read fn; do grep   ",'"$fn"'" side-deck; done >side-deck-filtered
 # dd doesn't support file tags, while cat does. 
-cat side-deck-filtered |dd conv=block cbs=80 bs=80 > zss.x
+cat side-deck-filtered |dd conv=block cbs=80 bs=80  |sed 's/\(.\{79\}\)./\1\n/g' >zss.x
 mkdir -p ../../lib
 cp zss.x ../../lib
 echo Side deck ../../lib/zss.x build successfully out of $FILES  
