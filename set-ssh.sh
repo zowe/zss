c1cb51cbda981095fd70199494b5466299f76dd2
Reet Chowdhary <rchowdhary@rocketsoftware.com>
2019-05-28 17:58:40 -0400

    added license
    Signed-off-by: Reet Chowdhary <rchowdhary@rocketsoftware.com>

--- set-ssh.sh
+++ set-ssh.sh
@@ -1,3 +1,11 @@
+#  This program and the accompanying materials are
+#  made available under the terms of the Eclipse Public License v2.0 which accompanies
+#  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+#  
+#  SPDX-License-Identifier: EPL-2.0
+#  
+#  Copyright Contributors to the Zowe Project.
+
 #! /bin/sh
 sed 's/https:\/\/github.com\/zowe\//git@github.com:zowe\//' .gitmodules > .gitmodules2 && mv .gitmodules2 .gitmodules
 git add .gitmodules
