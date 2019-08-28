7ce4fdd35f95b0bf0a54bba0749eae1a475b2b6f
Timothy Gerstel <tim.gerstel@gmail.com>
2019-08-28 11:57:00 -0400

    installServerStatusService now takes productVersion from zss.c.  Log route no longer needs separate function.  /server/agent response now conforms to HATEOAS principles.  Environment route makes use of ERBSMFI for CPU utilization
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- c/restService.c
+++ c/restService.c
@@ -26,6 +26,7 @@
 #include <string.h>
 #include <strings.h>
 #endif
+#include <sys/utsname.h>
 
 #include "zowetypes.h"
 #include "bpxnet.h"
@@ -37,12 +38,17 @@
 #include "logging.h"
 #include "zssLogging.h"
 #include "restService.h"
+#pragma linkage (EXSMFI,OS)
 
 #ifdef __ZOWE_OS_ZOS
 static int serveStatus(HttpService *service, HttpResponse *response);
 static JsonObject* serverConfig = NULL;
+char productVersion[40];
 
-int installServerStatusService(HttpServer *server, JsonObject *serverSettings)
+typedef int EXSMFI();
+EXSMFI *smf_func;
+
+int installServerStatusService(HttpServer *server, JsonObject *serverSettings, char* productVer)
 {
   HttpService *httpService = makeGeneratedService("REST_Service", "/server/agent/**");
   httpService->authType = SERVICE_AUTH_NATIVE_WITH_SESSION_TOKEN;
@@ -51,6 +57,7 @@ int installServerStatusService(HttpServer *server, JsonObject *serverSettings)
   httpService->doImpersonation = TRUE;
   registerHttpService(server, httpService);
   serverConfig = serverSettings;
+  memcpy(productVersion, productVer, 40);
   return 0;
 }
 
@@ -60,33 +67,40 @@ void respondWithServerConfig(HttpResponse *response){
   setDefaultJSONRESTHeaders(response);
   writeHeader(response);
   jsonStart(out);
+  jsonStartObject(out, "options");
   jsonPrintObject(out, serverConfig);
+  jsonEndObject(out);
   jsonEnd(out);
   finishResponse(response);
 }
 
-#ifndef MAX_LOG_LINE_LENGTH
-#define MAX_LOG_LINE_LENGTH 1024
-#endif
-void respondWithServerLog(HttpResponse *response){
+void respondWithServerRoutes(HttpResponse *response){
   jsonPrinter *out = respondWithJsonPrinter(response);
-  FILE *log;
-  log = fopen(getenv("ZSS_LOG_FILE"), "rb");
-  char buffer[MAX_LOG_LINE_LENGTH];
   setResponseStatus(response, 200, "OK");
   setDefaultJSONRESTHeaders(response);
   writeHeader(response);
   jsonStart(out);
-  jsonStartArray(out, NULL);
-  if(log){
-    while(fgets(buffer, sizeof(buffer), log)) {
-      if(strlen(buffer) > 0){
-        buffer[strlen(buffer)-1] = 0;
-      }
-      jsonAddString(out, NULL, buffer);
-    }
-    fclose(log);
-  }
+  jsonStartArray(out, "links");
+  jsonStartObject(out, NULL);
+  jsonAddString(out, "href", "/server/agent/config");
+  jsonAddString(out, "rel", "config");
+  jsonAddString(out, "type", "GET");
+  jsonEndObject(out);
+  jsonStartObject(out, NULL);
+  jsonAddString(out, "href", "/server/agent/log");
+  jsonAddString(out, "rel", "log");
+  jsonAddString(out, "type", "GET");
+  jsonEndObject(out);
+  jsonStartObject(out, NULL);
+  jsonAddString(out, "href", "/server/agent/logLevels");
+  jsonAddString(out, "rel", "logLevels");
+  jsonAddString(out, "type", "GET");
+  jsonEndObject(out);
+  jsonStartObject(out, NULL);
+  jsonAddString(out, "href", "/server/agent/environment");
+  jsonAddString(out, "rel", "environment");
+  jsonAddString(out, "type", "GET");
+  jsonEndObject(out);
   jsonEndArray(out);
   jsonEnd(out);
   finishResponse(response);
@@ -107,18 +121,68 @@ void respondWithLogLevels(HttpResponse *response){
 }
 
 void respondWithServerEnvironment(HttpResponse *response){
+  /*Information about parameters for smc_func: https://www.ibm.com/support/knowledgecenter/SSLTBW_2.1.0/com.ibm.zos.v2r1.erbb700/smfp.htm#smfp*/
   jsonPrinter *out = respondWithJsonPrinter(response);
+  struct utsname unameRet;
+  uname(&unameRet);
+  char pid[64];
+  char ppid[64];
+  char dp[64];
+  char cpu_u[64];
+  char mvs_u[64];
+  char ziip_u[64];
+  char zaap_u[64];
+  char* buffer;
+  int rc = 0;
+  int reqtype = 0x00000005; //fullword. request type
+  int rectype = 0x0000004F; //SMF record type, only type 79 is supported
+  int subtype = 0x00000009; //SMF record subtype
+  int bufferlen = 247488; /* length of SMF record buffer */
+  int cpu_util = 0x00000000;
+  int demand_paging = 0x00000000;
+  int options = 0x00000000;
+  int mvs_srm = 0x00000000;
+  int zaap_util = 0x00000000;
+  int ziip_util = 0x00000000;
+  buffer = (char *)safeMalloc(bufferlen, "buffer");
+  memset(buffer, 0, bufferlen);
+  smf_func = (EXSMFI *)fetch("ERBSMFI");
+  rc = (*smf_func)(&reqtype,
+               &rectype,
+               &subtype,
+               buffer,
+               &bufferlen,
+               &cpu_util,
+               &demand_paging,
+               &options,
+               &mvs_srm,
+               &zaap_util,
+               &ziip_util);
+  sprintf(pid, "%d", getpid());
+  sprintf(ppid, "%d", getppid());
+  sprintf(dp, "%d", demand_paging);
+  sprintf(cpu_u, "%d%", cpu_util);
+  sprintf(mvs_u, "%d%", mvs_srm);
+  sprintf(zaap_u, "%d%", zaap_util);
+  sprintf(ziip_u, "%d%", ziip_util);
   setResponseStatus(response, 200, "OK");
   setDefaultJSONRESTHeaders(response);
   writeHeader(response);
   jsonStart(out);
-  jsonAddString(out, "server args", "args here");
-  jsonAddString(out, "platform", "platform here");
-  jsonAddString(out, "OS", "blah");
-  jsonAddString(out, "arch", "blah");
-  jsonAddString(out, "cpus", "blah");
-  jsonAddString(out, "total memory", "blah");
-  jsonAddString(out, "PID", "blah");
+  jsonAddString(out, "log directory", getenv("ZSS_LOG_FILE"));
+  jsonAddString(out, "agent name", "zss");
+  jsonAddString(out, "agent version", productVersion);
+  jsonAddString(out, "arch", unameRet.sysname);
+  jsonAddString(out, "OS release", unameRet.release);
+  jsonAddString(out, "hardware identifier", unameRet.machine);
+  jsonAddString(out, "hostname", unameRet.nodename);
+  jsonAddString(out, "Demand Paging Rate", dp); 
+  jsonAddString(out, "Standard CP CPU Utilization", cpu_u);
+  jsonAddString(out, "Standard CP MVS/SRM CPU Utilization", mvs_u);
+  jsonAddString(out, "ZAAP CPU Utilization", zaap_u);
+  jsonAddString(out, "ZIIP CPU Utilization", ziip_u);
+  jsonAddString(out, "PID", pid);
+  jsonAddString(out, "PPID", ppid);
   jsonEnd(out);
   finishResponse(response);
 }
@@ -127,11 +191,18 @@ static int serveStatus(HttpService *service, HttpResponse *response) {
   HttpRequest *request = response->request;
   if (!strcmp(request->method, methodGET)) {
     char *l1 = stringListPrint(request->parsedFile, 2, 1, "/", 0);
-    if (!strcmp(l1, "config")){
+    if(!strcmp(l1, "")){
+      respondWithServerRoutes(response);
+    }
+    else if (!strcmp(l1, "config")){
       respondWithServerConfig(response);
     }
     else if (!strcmp(l1, "log")) {
-      respondWithServerLog(response);
+      if(strcmp(getenv("ZSS_LOG_FILE"), "")){
+        respondWithUnixFile2(NULL, response, getenv("ZSS_LOG_FILE"), 0, 0, false);
+      } else {
+         respondWithError(response, HTTP_STATUS_NO_CONTENT, "Log not found");
+      }
     }
     else if (!strcmp(l1, "logLevels")) {
       respondWithLogLevels(response);
