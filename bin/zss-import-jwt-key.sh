fa09adf2f2050743d3aad8875be1a7a84541bc1b
Fyodor Kovin <fkovin@rocketsoftware.com>
2019-09-13 15:04:02 +0200

    Copyright headers for the shell scripts
    Signed-off-by: Fyodor Kovin <fkovin@rocketsoftware.com>

--- bin/zss-import-jwt-key.sh
+++ bin/zss-import-jwt-key.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# This program and the accompanying materials are
+# made available under the terms of the Eclipse Public License v2.0 which accompanies
+# this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+# 
+# SPDX-License-Identifier: EPL-2.0
+# 
+# Copyright Contributors to the Zowe Project.
 
 mktemp1()
 {
@@ -49,3 +56,11 @@ chmod go-rwx $TMP_KEYSTORE
 echo $TMP_KEYSTORE_PASSWORD |gskkyman -i -t $TOKEN -l jwtsecret -p $TMP_KEYSTORE
 
 >&2 echo "Done"
+
+# This program and the accompanying materials are
+# made available under the terms of the Eclipse Public License v2.0 which accompanies
+# this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+# 
+# SPDX-License-Identifier: EPL-2.0
+# 
+# Copyright Contributors to the Zowe Project.
