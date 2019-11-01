30961201267cabb3e0c5bff0d6c674f6bd8d0d82
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-11-01 13:11:05 -0400

    change packaging stage to use scripts in .pax folder
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

---
+++ .pax/pre-packaging.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+set -x
+
+################################################################################
+# This program and the accompanying materials are made available under the terms of the
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright IBM Corporation 2018, 2019
+################################################################################
+
+# contants
+SCRIPT_NAME=$(basename "$0")
+SCRIPT_DIR=$(dirname "$0")
+# these are folders on build server
+export JAVA_HOME=/usr/lpp/java/J8.0_64
+export ANT_HOME=/ZOWE/apache-ant-1.10.5
+export STEPLIB=CBC.SCCNCMP
+
+# build
+echo "$SCRIPT_NAME build zss ..."
+cd build && ${ANT_HOME}/bin/ant
+
+# clean up content folder
+echo "$SCRIPT_NAME cleaning up pax folder ..."
+cd "$SCRIPT_DIR"
+mv content bak && mkdir -p content
+
+# move real files to the content folder
+echo "$SCRIPT_NAME coping files should be in pax ..."
+cd content
+mkdir LOADLIB SAMPLIB &&
+  cp -X "//DEV.LOADLIB(ZWESIS01)" LOADLIB/ZWESIS01  &&
+  cp -X "//DEV.LOADLIB(ZWESAUX)" LOADLIB/ZWESAUX  &&
+  cp ../bak/samplib/zis/* SAMPLIB/  &&
+  cp ../bak/bin/zssServer ./  &&
+  extattr +p zssServer
