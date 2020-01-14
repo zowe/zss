e758fe9f249d677b85317253c139b0522ce8fb67
Timothy Gerstel <tim.gerstel@gmail.com>
2020-01-14 11:59:18 -0500

    Remove dataset writing code that somehow made it in
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- h/datasetService.h
+++ h/datasetService.h
@@ -13,7 +13,6 @@
 #ifndef __DATASET_SERVICE_H__
 #define __DATASET_SERVICE_H__
 
-void installDatasetWritingService(HttpServer *server);
 void installDatasetContentsService(HttpServer *server);
 void installVSAMDatasetContentsService(HttpServer *server);
 void installDatasetMetadataService(HttpServer *server);
