8e4347120a10c48813e698d23b2ab85fbe1dd6cb
Leanid Astrakou <lastrakou@rocketsoftware.com>
2020-08-18 10:55:51 -0400

    Add registration code to configure script for ML
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- bin/configure.sh
+++ bin/configure.sh
@@ -27,3 +27,39 @@ export NODE_PATH=../..:../../zlux-server-framework/node_modules:$NODE_PATH
 __UNTAGGED_READ_MODE=V6 $NODE_BIN initInstance.js
 
 APP_WORKSPACE_DIR=${INSTANCE_DIR}/workspace/app-server
+
+# Add static definition for zss
+cat <<EOF >${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml
+#
+services:
+  - serviceId: zss
+    title: Zowe System Services Server
+    description: Zowe System Services Server (ZSS) is used for enabling low-level microservices and other privileged services
+    catalogUiTileId: zss
+    instanceBaseUrls:
+      - https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/
+    homePageRelativeUrl:
+    routedServices:
+      - gatewayUrl: api/v1
+        serviceRelativeUrl: api/v1/jobs
+      - gatewayUrl: api/v2
+        serviceRelativeUrl: api/v2/jobs
+ apiInfo:
+      - apiId: com.ibm.jobs.v1
+        gatewayUrl: api/v1
+        version: 1.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
+      - apiId: com.ibm.jobs.v2
+        gatewayUrl: api/v2
+        version: 2.0.0
+        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
+        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
+catalogUiTiles:
+  jobs:
+    title: Zowe System Services Server (ZSS)
+    description:  Zowe System Services Server for enabling low-level microservices and other privileged services
+EOF
+iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml > $STATIC_DEF_CONFIG_DIR/zss.yml
+rm ${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml
+chmod 770 $STATIC_DEF_CONFIG_DIR/zss.yml
