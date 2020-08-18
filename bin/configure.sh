78b6e3653df71f8b4fa5a80d2e239a853292fda7
Leanid Astrakou <lastrakou@rocketsoftware.com>
2020-08-18 12:12:50 -0400

    Removed sample data from jobs api
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- bin/configure.sh
+++ bin/configure.sh
@@ -28,7 +28,7 @@ __UNTAGGED_READ_MODE=V6 $NODE_BIN initInstance.js
 
 APP_WORKSPACE_DIR=${INSTANCE_DIR}/workspace/app-server
 
-# Add static definition for zss
+# Add static definition for zss. TODO: Needs routedServices + apiInfo
 cat <<EOF >${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml
 #
 services:
@@ -37,26 +37,12 @@ services:
     description: Zowe System Services Server (ZSS) is used for enabling low-level microservices and other privileged services
     catalogUiTileId: zss
     instanceBaseUrls:
-      - https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/
+      - https://${ZOWE_EXPLORER_HOST}:${ZOWE_ZSS_SERVER_PORT}/
     homePageRelativeUrl:
     routedServices:
-      - gatewayUrl: api/v1
-        serviceRelativeUrl: api/v1/jobs
-      - gatewayUrl: api/v2
-        serviceRelativeUrl: api/v2/jobs
- apiInfo:
-      - apiId: com.ibm.jobs.v1
-        gatewayUrl: api/v1
-        version: 1.0.0
-        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
-        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
-      - apiId: com.ibm.jobs.v2
-        gatewayUrl: api/v2
-        version: 2.0.0
-        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
-        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
+    apiInfo:
 catalogUiTiles:
-  jobs:
+  zss:
     title: Zowe System Services Server (ZSS)
     description:  Zowe System Services Server for enabling low-level microservices and other privileged services
 EOF
