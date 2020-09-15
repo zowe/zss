8f74f03cbb7f788b8daebc3094c6d3ffbd3cc97f
Leanid Astrakou <lastrakou@rocketsoftware.com>
2020-09-15 11:26:45 -0400

    Addressed code review
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- bin/configure.sh
+++ bin/configure.sh
@@ -33,75 +33,27 @@ cat <<EOF >${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml
 #
 services:
   - serviceId: zss
-    title: Zowe System Services Server (ZSS)
-    description: Zowe System Services Server (ZSS) is used for enabling low-level microservices and other privileged services
+    title: Zowe System Services (ZSS)
+    description: Zowe System Services is used for enabling low-level microservices and other privileged mainframe services, 
+    like USS, MVS, authentication, and security management.
     catalogUiTileId: zss
     instanceBaseUrls:
       - https://${ZOWE_EXPLORER_HOST}:${ZOWE_ZSS_SERVER_PORT}/
     homePageRelativeUrl:
     routedServices:
-      - gatewayUrl: /server/agent/environment # Displays ZSS host environment info
-        serviceRelativeUrl: /server/agent/environment 
-      - gatewayUrl: /server/agent/services  # Returns list of ZSS services
-        serviceRelativeUrl: /server/agent/services
-      - gatewayUrl: /plugins
-        serviceRelativeUrl: /plugins
-      - gatewayUrl: /datasetMetadata/
-        serviceRelativeUrl: /datasetMetadata/
-      - gatewayUrl: /datasetContents/
-        serviceRelativeUrl: /datasetContents/
-      - gatewayUrl: /security-mgmt/classes/
-        serviceRelativeUrl: /security-mgmt/classes/
-      - gatewayUrl: /security-mgmt/access/
-        serviceRelativeUrl: /security-mgmt/access/
-      - gatewayUrl: /security-mgmt/groups/
-        serviceRelativeUrl: /security-mgmt/groups/
-      - gatewayUrl: /security-mgmt/user-profiles/
-        serviceRelativeUrl: /security-mgmt/user-profiles/
-      - gatewayUrl: /saf-auth/
-        serviceRelativeUrl: /saf-auth/
-      - gatewayUrl: /VSAMdatasetContents/
-        serviceRelativeUrl: /VSAMdatasetContents/
-      - gatewayUrl: /unixfile/contents
-        serviceRelativeUrl: /unixfile/contents
-      - gatewayUrl: /unixfile/chmod/
-        serviceRelativeUrl: /unixfile/chmod/
-      - gatewayUrl: /unixfile/chtag/
-        serviceRelativeUrl: /unixfile/chtag/
-      - gatewayUrl: /unixfile/chown/
-        serviceRelativeUrl: /unixfile/chown/
-      - gatewayUrl: /unixfile/metadata/
-        serviceRelativeUrl: /unixfile/metadata/
-      - gatewayUrl: /unixfile/touch/
-        serviceRelativeUrl: /unixfile/touch/
-      - gatewayUrl: /unixfile/mkdir/
-        serviceRelativeUrl: /unixfile/mkdir/
-      - gatewayUrl: /unixfile/copy/
-        serviceRelativeUrl: /unixfile/copy/
-      - gatewayUrl: /unixfile/rename/
-        serviceRelativeUrl: /unixfile/rename/
-      - gatewayUrl: /login/
-        serviceRelativeUrl: /login/
-      - gatewayUrl: /logout/
-        serviceRelativeUrl: /logout/
-      - gatewayUrl: /ras/
-        serviceRelativeUrl: /ras/
-      - gatewayUrl: /password/
-        serviceRelativeUrl: /password/
-      - gatewayUrl: /omvs/
-        serviceRelativeUrl: /omvs/
-      - gatewayUrl: /unixfile/
-        serviceRelativeUrl: /unixfile/
+      - gatewayUrl: api/v1
+        serviceRelativeUrl: 
     apiInfo:
       - apiId: org.zowe.zss
-        gatewayUrl: server/agent/
+        gatewayUrl: api/v1
         version: 1.15.0+20200901
         # swaggerUrl: TODO
         # documentationUrl: TODO
 catalogUiTiles:
   zss:
-    title: Zowe System Services Server (ZSS)
-    description:  Zowe System Services Server for enabling low-level microservices and other privileged services
+    title: Zowe System Services (ZSS)
+    description:  Zowe System Services is used for enabling low-level microservices and other privileged mainframe services, 
+    like USS, MVS, authentication, and security management.
 EOF
 iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml > $STATIC_DEF_CONFIG_DIR/zss.yml
 rm ${STATIC_DEF_CONFIG_DIR}/zss.ebcidic.yml
