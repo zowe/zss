9b80749ada5a9e60a994c883064dda717927cdb1
Leanid Astrakou <lastrakou@rocketsoftware.com>
2021-01-12 22:24:05 -0500

    More code review adjustments
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- c/zss.c
+++ c/zss.c
@@ -1233,8 +1233,10 @@ int main(int argc, char **argv){
     strcat(serverTimeoutsDir, serverTimeoutsDirSuffix);
     htUsers = getServerUserTimeoutsHt(slh, serverTimeoutsDir, "users");
     htGroups = getServerUserTimeoutsHt(slh, serverTimeoutsDir, "groups");
+    printf("timeoutsdir: %s  instanceDir: %s   serverTimeoutsDirSuffix: %s", serverTimeoutsDir, instanceDir, serverTimeoutsDirSuffix);
     safeFree(serverTimeoutsDir, serverTimeoutsDirSize);
-    
+    printf("timeoutsdir: %s  instanceDir: %s   serverTimeoutsDirSuffix: %s", serverTimeoutsDir, instanceDir, serverTimeoutsDirSuffix);
+
     /* This one IS used*/
     checkAndSetVariableWithEnvOverride(mvdSettings, "pluginsDir", envSettings, "ZWED_pluginsDir", pluginsDir, COMMON_PATH_MAX);
 
