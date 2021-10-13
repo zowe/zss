a2d2349ae3ec88512fd5ead9d052475618b4f9e5
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-10-13 09:13:52 +0500

    Add copyright
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- c/jwk.c
+++ c/jwk.c
@@ -1,3 +1,14 @@
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
 #ifdef METTLE
 #error Metal C not supported
 #endif // METTLE
@@ -259,3 +270,14 @@ int checkJwtSignature(JwsAlgorithm algorithm,
   }
   return RC_JWT_OK;
 }
+
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
