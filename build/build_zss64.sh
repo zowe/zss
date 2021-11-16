e219f66e5cf23e8487175785ad84c1983c429031
Vit Tomica <vit.tomica@broadcom.com>
2021-11-16 12:47:54 +0100

    update the build_zss64.sh script
    Signed-off-by: Vit Tomica <vit.tomica@broadcom.com>

--- build/build_zss64.sh
+++ build/build_zss64.sh
@@ -122,6 +122,7 @@ if ! c89 \
   ${ZSS}/c/rasService.c \
   ${ZSS}/c/userInfoService.c \
   ${ZSS}/c/passTicketService.c \
+  ${ZSS}/c/safIdtService.c \
   ${GSKLIB} ;
 then
   extattr +p ${ZSS}/bin/zssServer64
