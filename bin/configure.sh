a17069bac693cd337f69d0037fe9ede0b08e7e6b
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-12-16 09:46:17 +0500

    Fixes for env vars and +x attribute for scripts
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- bin/configure.sh
+++ bin/configure.sh
@@ -9,8 +9,56 @@
 
 
 # Required variables on shell:
-# - ROOT_DIR
-# - WORKSPACE_DIR
+# - ZWE_zowe_runtimeDirectory
+# - ZWE_zowe_workspaceDirectory
+convert_v2_to_v1() {
+  while read old_name new_name; do
+    old_val=$(eval echo "\$${old_name}")
+    new_val=$(eval echo "\$${new_name}")
+    if [ -z "${old_val}" -a -n "${new_val}" ]; then
+      export "${old_name}=${new_val}"
+    fi
+  done <<EOF
+CATALOG_PORT ZWE_components_api_catalog_port 
+DISCOVERY_PORT ZWE_components_discovery_port
+GATEWAY_HOST ZWE_haInstance_hostname
+GATEWAY_PORT ZWE_components_gateway_port
+KEY_ALIAS ZWE_zowe_certificate_keystore_alias
+KEYSTORE ZWE_zowe_certificate_keystore_file
+KEYSTORE_CERTIFICATE ZWE_zowe_certificate_pem_certificate
+KEYSTORE_CERTIFICATE_AUTHORITY ZWE_zowe_certificate_pem_certificateAuthority
+KEYSTORE_CERTIFICATE_AUTHORITY ZWE_zowe_certificate_truststore_certificateAuthorities
+KEYSTORE_DIRECTORY ZWE_zowe_setup_certificate_pkcs12_directory
+KEYSTORE_KEY ZWE_zowe_certificate_pem_key
+KEYSTORE_PASSWORD ZWE_zowe_certificate_keystore_password
+KEYSTORE_TYPE ZWE_zowe_certificate_keystore_type
+ROOT_DIR ZWE_zowe_runtimeDirectory
+TRUSTSTORE ZWE_zowe_certificate_truststore_file
+VERIFY_CERTIFICATES ZWE_zowe_verifyCertificates
+WORKSPACE_DIR ZWE_zowe_workspaceDirectory
+ZOWE_EXPLORER_HOST ZWE_haInstance_hostname
+ZOWE_PREFIX ZWE_zowe_job_prefix
+ZOWE_ZLUX_SERVER_HTTPS_PORT ZWE_components_app_server_port
+ZOWE_ZSS_XMEM_SERVER_NAME ZWE_components_zss_crossMemoryServerName
+ZWED_agent_https_keyring ZWE_zowe_certificate_keystore_file
+ZWED_agent_https_label ZWE_zowe_certificate_keystore_alias
+ZWED_agent_https_password ZWE_zowe_certificate_keystore_password
+ZWED_node_https_port ZWE_components_app_server_port
+ZWES_SERVER_PORT ZWE_components_zss_port
+ZWES_SERVER_TLS ZWE_components_zss_tls
+EOF
+}
+
+# where do we get??
+# ZOWE_IP_ADDRESS
+# ZWES_ZIS_LOADLIB TS3105.SZWEAUTH
+# ZWES_ZIS_PLUGINLIB TS3105.SZWEPLUG
+# ZWES_ZIS_PARMLIB TS3105.SZWESAMP
+# ZWES_ZIS_PARMLIB_MEMBER ZWESIP00
+
+convert_v2_to_v1
+
+export ZWED_agent_https_ipAddresses="0.0.0.0"
 
 cd ${ROOT_DIR}/components/app-server/share/zlux-app-server/bin
 . ./convert-env.sh
