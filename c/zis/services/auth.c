5cede98a50eea7cb494da98c953690c403375cd1
achmelo <a.chmelo@gmail.com>
2022-05-03 13:56:29 +0200

    stack variable again

--- c/zis/services/auth.c
+++ c/zis/services/auth.c
@@ -73,21 +73,19 @@ static int handleGenerateToken(AuthServiceParmList *parmList,
    int safRC = 0, racfRC = 0, racfRsn = 0;
    int deleteSAFRC = 0, deleteRACFRC = 0, deleteRACFRsn = 0;
    int rc = RC_ZIS_AUTHSRV_OK;
-   IDTA *idta = NULL;
    int options = VERIFY_CREATE;
 
-    idta = (IDTA *) safeMalloc31(sizeof(IDTA), "Idta structure");
-
-    memset(idta, 0, sizeof(IDTA));
-    memcpy(idta->id, "IDTA", 4);
-    idta->version = IDTA_VERSION_0001;
-    idta->length = sizeof(IDTA);
-    idta->idtType = IDTA_JWT_IDT_Type;
-    idta->idtBufferPtr = parmList->safIdtService.safIdt;
-    idta->idtBufferLen = sizeof(parmList->safIdtService.safIdt);
-    idta->idtLen = parmList->safIdtService.safIdtLen;
-    idta->idtPropIn = IDTA_End_User_IDT;
-    options |= VERIFY_GENERATE_IDT;
+   IDTA idtaVar;
+   memset(&idtaVar, 0, sizeof(idtaVar));
+   idtaVar.version = IDTA_VERSION_0001;
+   idtaVar.length = sizeof(IDTA);
+   idtaVar.idtBufferPtr = parmList->safIdtService.safIdt;
+   idtaVar.idtBufferLen = sizeof(parmList->safIdtService.safIdt);
+   idtaVar.idtLen = parmList->safIdtService.safIdtLen;
+   idtaVar.idtType = IDTA_JWT_IDT_Type;
+   idtaVar.idtPropIn = IDTA_End_User_IDT;
+   memcpy(idtaVar.id, "IDTA", 4);
+   IDTA *idta = &idtaVar;
 
   CMS_DEBUG(globalArea, "handleGenerateToken(): username = %s, password = %s\n",
       parmList->userIDNullTerm, "******");
