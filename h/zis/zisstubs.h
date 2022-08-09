2aea1230f8f0f66aa2737746dbb517d0bcde0eb0
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2022-08-09 20:54:35 +0200

    Add copyright.
    Signed-off-by: Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>

--- h/zis/zisstubs.h
+++ h/zis/zisstubs.h
@@ -1,3 +1,14 @@
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 #ifndef __ZOSSTEAM_ZISSTUBS__
 #define __ZOSSTEAM_ZISSTUBS__ 1
 
@@ -409,3 +420,13 @@
 
 void dynamicZISUndefinedStub(void);
 int dynamicZISVersion(void);
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
