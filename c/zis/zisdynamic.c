bfe0b7b0cb76a45d617c1b1832aeaaea0f698d07
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2022-08-09 20:54:35 +0200

    Add copyright.
    Signed-off-by: Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>

--- c/zis/zisdynamic.c
+++ c/zis/zisdynamic.c
@@ -1,3 +1,14 @@
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 #include <metal/metal.h>
 #include <metal/stddef.h>
 #include <metal/stdint.h>
@@ -649,3 +660,13 @@ static void assignSlots(void **stubVector) {
   stubVector[ZIS_STUB_LPADEL  ] = (void*)lpaDelete;
   stubVector[ZIS_STUB_CMADDPRM] = (void*)cmsAddConfigParm;
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
