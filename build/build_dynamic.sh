113b40652c3d9db116f6a020f88fbfc3644014d1
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2022-08-22 13:28:26 +0200

    Use sh for zis dyn build.
    Signed-off-by: Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>

--- build/build_dynamic.sh
+++ build/build_dynamic.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 ################################################################################
 #  This program and the accompanying materials are
@@ -10,131 +11,109 @@
 #  Copyright Contributors to the Zowe Project.
 ################################################################################
 
-COMMON=../deps/zowe-common-c
-ZSS=..
+export _LD_SYSLIB="//'SYS1.CSSLIB'://'CEE.SCEELKEX'://'CEE.SCEELKED'://'CEE.SCEERUN'://'CEE.SCEERUN2'://'CSF.SCSFMOD0'"
 
-CFLAGS=(-S -M -qmetal -q64 -DSUBPOOL=132 -DMETTLE=1 -DMSGPREFIX='"IDX"'
--DHAVE_METALIO=1
--qreserved_reg=r12
--Wc,"arch(8),agg,exp,list(),so(),off,xref,roconst,longname,lp64" 
--I $COMMON/h -I $ZSS/h -I $ZSS/h -I .)
+WORKING_DIR=$(dirname "$0")
+ZSS="../.."
+COMMON="../../deps/zowe-common-c"
 
-ASFLAGS=(-mgoff -mobject -mflag=nocont --TERM --RENT)
-
-LDFLAGS=(-V -b ac=1 -b rent -b case=mixed -b map -b xref -b reus)
 
 echo "********************************************************************************"
 echo "Building ZIS Dynamic Linkage Base plugin..."
+
+mkdir -p "${WORKING_DIR}/tmp-zisdyn" && cd "$_"
 
 IFS='.' read -r major minor micro < "${ZSS}/version.txt"
 date_stamp=$(date +%Y%m%d)
 echo "Version: $major.$minor.$micro"
 echo "Date stamp: $date_stamp"
 
-xlc "${CFLAGS[@]}" -DRCVR_CPOOL_STATES \
+xlc -S -M -qmetal -q64 -DSUBPOOL=132 -DMETTLE=1 -DMSGPREFIX=\"ZWE\" \
   -DZISDYN_MAJOR_VERSION="$major" \
   -DZISDYN_MINOR_VERSION="$minor" \
   -DZISDYN_REVISION="$micro" \
   -DZISDYN_VERSION_DATE_STAMP="$date_stamp" \
-$COMMON/c/alloc.c \
-$COMMON/c/cmutils.c \
-$COMMON/c/collections.c \
-$COMMON/c/cpool64.c \
-$COMMON/c/crossmemory.c \
-$COMMON/c/logging.c \
-$COMMON/c/metalio.c \
-$COMMON/c/qsam.c \
-$COMMON/c/timeutls.c \
-$COMMON/c/utils.c \
-$COMMON/c/xlate.c \
-$COMMON/c/le.c \
-$COMMON/c/logging.c \
-$COMMON/c/recovery.c \
-$COMMON/c/scheduling.c \
-$COMMON/c/pause-element.c \
-$COMMON/c/shrmem64.c \
-$COMMON/c/zos.c \
-$COMMON/c/zvt.c \
-$COMMON/c/cellpool.c \
-$COMMON/c/nametoken.c \
-$COMMON/c/lpa.c \
-$COMMON/c/mtlskt.c \
-$COMMON/c/stcbase.c \
-$COMMON/c/isgenq.c \
-$COMMON/c/resmgr.c \
-$ZSS/c/zis/plugin.c \
-$ZSS/c/zis/server-api.c \
-$ZSS/c/zis/service.c \
-$ZSS/c/zis/client.c \
-$ZSS/c/zis/parm.c \
-$ZSS/c/zis/zisdynamic.c
+  -qreserved_reg=r12 \
+  -DRCVR_CPOOL_STATES \
+  -DHAVE_METALIO=1 \
+  -Wc,arch\(8\),agg,exp,list\(\),so\(\),off,xref,roconst,longname,lp64 \
+  -I ${COMMON}/h -I ${ZSS}/h \
+  ${COMMON}/c/alloc.c \
+  ${COMMON}/c/cellpool.c \
+  ${COMMON}/c/cmutils.c \
+  ${COMMON}/c/collections.c \
+  ${COMMON}/c/cpool64.c \
+  ${COMMON}/c/crossmemory.c \
+  ${COMMON}/c/logging.c \
+  ${COMMON}/c/metalio.c \
+  ${COMMON}/c/qsam.c \
+  ${COMMON}/c/timeutls.c \
+  ${COMMON}/c/utils.c \
+  ${COMMON}/c/xlate.c \
+  ${COMMON}/c/le.c \
+  ${COMMON}/c/logging.c \
+  ${COMMON}/c/recovery.c \
+  ${COMMON}/c/scheduling.c \
+  ${COMMON}/c/pause-element.c \
+  ${COMMON}/c/shrmem64.c \
+  ${COMMON}/c/zos.c \
+  ${COMMON}/c/zvt.c \
+  ${COMMON}/c/nametoken.c \
+  ${COMMON}/c/lpa.c \
+  ${COMMON}/c/mtlskt.c \
+  ${COMMON}/c/stcbase.c \
+  ${COMMON}/c/isgenq.c \
+  ${COMMON}/c/resmgr.c \
+  ${ZSS}/c/zis/plugin.c \
+  ${ZSS}/c/zis/server-api.c \
+  ${ZSS}/c/zis/service.c \
+  ${ZSS}/c/zis/client.c \
+  ${ZSS}/c/zis/parm.c \
+  ${ZSS}/c/zis/zisdynamic.c
 
-as "${ASFLAGS[@]}" -aegimrsx=alloc.asm alloc.s
-as "${ASFLAGS[@]}" -aegimrsx=cmutils.asm cmutils.s
-as "${ASFLAGS[@]}" -aegimrsx=collections.asm collections.s
-as "${ASFLAGS[@]}" -aegimrsx=cpool64.asm cpool64.s
-as "${ASFLAGS[@]}" -aegimrsx=crossmemory.asm crossmemory.s
-as "${ASFLAGS[@]}" -aegimrsx=logging.asm logging.s
-as "${ASFLAGS[@]}" -aegimrsx=metalio.asm metalio.s
-as "${ASFLAGS[@]}" -aegimrsx=qsam.asm qsam.s
-as "${ASFLAGS[@]}" -aegimrsx=timeutls.asm timeutls.s
-as "${ASFLAGS[@]}" -aegimrsx=utils.asm utils.s
-as "${ASFLAGS[@]}" -aegimrsx=zos.asm zos.s
-as "${ASFLAGS[@]}" -aegimrsx=le.asm le.s
-as "${ASFLAGS[@]}" -aegimrsx=recovery.asm recovery.s
-as "${ASFLAGS[@]}" -aegimrsx=scheduling.asm scheduling.s
-as "${ASFLAGS[@]}" -aegimrsx=pause-element.asm pause-element.s
-as "${ASFLAGS[@]}" -aegimrsx=shrmem64.asm shrmem64.s
-as "${ASFLAGS[@]}" -aegimrsx=xlate.asm xlate.s
-as "${ASFLAGS[@]}" -aegimrsx=zvt.asm zvt.s
-as "${ASFLAGS[@]}" -aegimrsx=cellpool.asm cellpool.s
-as "${ASFLAGS[@]}" -aegimrsx=nametoken.asm nametoken.s
-as "${ASFLAGS[@]}" -aegimrsx=lpa.asm lpa.s
-as "${ASFLAGS[@]}" -aegimrsx=mtlskt.asm mtlskt.s
-as "${ASFLAGS[@]}" -aegimrsx=stcbase.asm stcbase.s
-as "${ASFLAGS[@]}" -aegimrsx=isgenq.asm isgenq.s
-as "${ASFLAGS[@]}" -aegimrsx=resmgr.asm resmgr.s
-as "${ASFLAGS[@]}" -aegimrsx=plugin.asm plugin.s
-as "${ASFLAGS[@]}" -aegimrsx=server-api.asm server-api.s
-as "${ASFLAGS[@]}" -aegimrsx=service.asm service.s
-as "${ASFLAGS[@]}" -aegimrsx=client.asm client.s
-as "${ASFLAGS[@]}" -aegimrsx=parm.asm parm.s
-as "${ASFLAGS[@]}" -aegimrsx=zisdynamic.asm zisdynamic.s
+for file in \
+    alloc cellpool cmutils collections cpool64 crossmemory isgenq le logging lpa metalio mtlskt nametoken \
+    pause-element qsam recovery resmgr scheduling shrmem64 stcbase timeutls utils xlate \
+    zos zvt \
+    client parm plugin server-api service \
+    zisdynamic
+do
+  as -mgoff -mobject -mflag=nocont --TERM --RENT -aegimrsx=${file}.asm ${file}.s
+done
 
-export _LD_SYSLIB="//'SYS1.CSSLIB'://'CEE.SCEELKEX'://'CEE.SCEELKED'://'CEE.SCEERUN'://'CEE.SCEERUN2'://'CSF.SCSFMOD0'"
 
-ld "${LDFLAGS[@]}" -e getPluginDescriptor \
+ld -V -b ac=1 -b rent -b case=mixed -b map -b xref -b reus -e getPluginDescriptor \
 -o "//'$USER.DEV.OMDS.TEST.LOADLIB(ZWESISDL)'" \
 alloc.o \
+cellpool.o \
 cmutils.o \
 collections.o \
 cpool64.o \
 crossmemory.o \
+isgenq.o \
 le.o \
 logging.o \
+lpa.o \
 metalio.o \
-qsam.o \
+mtlskt.o \
+nametoken.o \
 pause-element.o \
+qsam.o \
 recovery.o \
-shrmem64.o \
 scheduling.o \
+shrmem64.o \
+stcbase.o \
 timeutls.o \
 utils.o \
 xlate.o \
 zos.o \
 zvt.o \
-cellpool.o \
-nametoken.o \
-lpa.o \
-mtlskt.o \
-stcbase.o \
-isgenq.o \
 resmgr.o \
+client.o \
+parm.o \
 plugin.o \
 server-api.o \
 service.o \
-client.o \
-parm.o \
 zisdynamic.o \
 > ZWESISDL.link
 
