5ee241eda41e50d93d01a4e88328b2cb55722daf
Tim Gerstel <tgerstel@rocketsoftware.com>
2023-01-19 01:14:38 -0500

    test case provided kindly by irek f.
    Signed-off-by: Tim Gerstel <tgerstel@rocketsoftware.com>

--- plugins/zis/ztop-bpxkil/test.c
+++ plugins/zis/ztop-bpxkil/test.c
@@ -33,10 +33,79 @@
 #define RC_OK     0
 #define RC_ERROR  8
 
-int main(int argc, char **argv) {
-
-  return RC_OK;
+ 
+static bool isPositiveNum(const char *str) {
+  const char *currChar = str;
+  while (*currChar != '\0') {
+    if (!isdigit(*currChar)) {
+      return false;
+    }
+    currChar++;
+  }
+  return true;
 }
+
+
+ int main(int argc, char **argv) {
+ 
+  if (argc < 3) {
+    printf("error: bad args\n");
+    printf("info: use > test <zis_name> <pid> <signal>\n");
+    return RC_ERROR;
+  }
+
+  char *serverNameArg = argv[1];
+  char *pidStr = argv[2];
+  char *signalStr = argv[3];
+  if (!isPositiveNum(pidStr)) {
+    printf("error: bad pid arg\n");
+    return RC_ERROR;
+  }
+  if (!isPositiveNum(signalStr)) {
+    printf("error: bad signal arg\n");
+    return RC_ERROR;
+  }
+  int pid = atoi(pidStr);
+  int sig = atoi(signalStr);
+
+  printf("info: server name = '%s', pid = %d, signal = %d'\n", serverNameArg, pid, sig);
+
+  if (pid <= 1) {
+    printf("error: pid too low, use pids > 1\n");
+    return RC_ERROR;
+  }
+  if (sig < 1) {
+    printf("error: bad signal value, use signals >= 1\n");
+    return RC_ERROR;
+  }
+
+  CrossMemoryServerName serverName = cmsMakeServerName(serverNameArg);
+
+  EchoServiceParmList parmlist = {
+      .eyecatcher = ECHO_SERVICE_PARMLIST_EYECATCHER,
+      .pidInt = pid,
+      .signalInt = sig,
+  };
+
+  ZISServiceStatus status = {0};
+
+  int rc = RC_ZIS_SRVC_OK;
+
+  ZISServicePath path1 = {
+      .pluginName = "ZTOP_ZIS        ",
+      .serviceName.text = "ZTOP_KILL       ",
+  };
+
+  rc = zisCallService(&serverName, &path1, &parmlist, &status);
+  if (rc != RC_ZIS_SRVC_OK) {
+    printf("error: cross-memory rc = %d, status:\n", rc);
+    dumpbuffer((char *)&status, sizeof(status));
+    return RC_ERROR;
+  }
+  printf("info: the test has completed successfully\n");
+
+   return RC_OK;
+ }
 
 
 /*
