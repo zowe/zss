ef4b364cf1ace7661b41b5f72b5304fd4f43c25d
1000TurquoisePogs <sgrady@rocketsoftware.com>
2023-02-02 13:38:27 -0500

    Update prepare-workspace.sh
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- .pax/prepare-workspace.sh
+++ .pax/prepare-workspace.sh
@@ -38,16 +38,3 @@ rsync -rv \
   --prune-empty-dirs --remove-source-files \
   "${PAX_WORKSPACE_DIR}/ascii/" \
   "${PAX_WORKSPACE_DIR}/content/"
-
-# update build information
-# BRANCH_NAME and BUILD_NUMBER is Jenkins environment variable
-commit_hash=$(git rev-parse --verify HEAD)
-current_timestamp=$(date +%s%3N)
-sed -e "s|{{build\.branch}}|${BRANCH_NAME}|g" \
-    -e "s|{{build\.number}}|${BUILD_NUMBER}|g" \
-    -e "s|{{build\.commitHash}}|${commit_hash}|g" \
-    -e "s|{{build\.timestamp}}|${current_timestamp}|g" \
-    "manifest.template.yaml" > "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml.tmp"
-mv "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml.tmp" "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml"
-echo "[${SCRIPT_NAME}] manifest:"
-cat "${PAX_WORKSPACE_DIR}/ascii/manifest.yaml"
