657bdaabf56fc52b1298592e82ad24a5e986a593
Sakshi Bobade <bobadesakshi21@gmail.com>
2023-04-21 18:09:55 +0530

    bugfix/space-alloc-unit

--- c/datasetjson.c
+++ c/datasetjson.c
@@ -446,7 +446,7 @@ static void addDetailsFromDSCB(char *dscb, jsonPrinter *jPrinter, int *isPDS) {
     char spac = (dscb[94-posOffset]);
     int sizeType = DATASET_ALLOC_TYPE_BYTE;
     if ((spac & 0xc0) == 0xc0){
-      jsonAddString(jPrinter, "space", "cyls");
+      jsonAddString(jPrinter, "space", "CYL");
       sizeType=DATASET_ALLOC_TYPE_CYL;
     } else if (spac & 0x80){
       if (spac & 0x10){
@@ -455,15 +455,15 @@ static void addDetailsFromDSCB(char *dscb, jsonPrinter *jPrinter, int *isPDS) {
           zowelog(NULL, LOG_COMP_RESTDATASET, ZOWE_LOG_DEBUG, "scxtv original blklen!\n");
         }
         if (cxtf & 0x40){
-          jsonAddString(jPrinter, "space", "mb");
+          jsonAddString(jPrinter, "space", "MB");
           sizeType=DATASET_ALLOC_TYPE_MB;
           primarySizeDiv = 1048576;
         } else if (cxtf & 0x20){
-          jsonAddString(jPrinter, "space", "kb");
+          jsonAddString(jPrinter, "space", "KB");
           sizeType=DATASET_ALLOC_TYPE_KB;
           primarySizeDiv = 1024;
         } else if (cxtf & 0x10){
-          jsonAddString(jPrinter, "space", "bytes");
+          jsonAddString(jPrinter, "space", "BYTE");
         }
         if (cxtf & 0x08){
           scxtvMult=256;
@@ -472,15 +472,15 @@ static void addDetailsFromDSCB(char *dscb, jsonPrinter *jPrinter, int *isPDS) {
           scxtvMult=65536;
         }
       } else{
-        jsonAddString(jPrinter, "space", "trks");
+        jsonAddString(jPrinter, "space", "TRK");
         sizeType=DATASET_ALLOC_TYPE_TRK;
       }
     } else if (spac & 0x40){
-      jsonAddString(jPrinter, "space", "blks");
+      jsonAddString(jPrinter, "space", "BLK");
       sizeType=DATASET_ALLOC_TYPE_BLOCK;
       primarySizeDiv = blockSize;
     } else{
-      jsonAddString(jPrinter, "space", "trks");
+      jsonAddString(jPrinter, "space", "TRK");
       sizeType=DATASET_ALLOC_TYPE_TRK;
       zowelog(NULL, LOG_COMP_RESTDATASET, ZOWE_LOG_DEBUG, "spac ABS!\n");
     }
