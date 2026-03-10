f5224dff777dc9965653a9468b0b2c523a77d1eb
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-03-10 14:19:12 -0400

    Update build_zss64.sh with new file
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- build/build_zss64.sh
+++ build/build_zss64.sh
@@ -252,6 +252,7 @@ if ! c89 \
   ${ZSS}/c/passTicketService.c \
   ${ZSS}/c/jwk.c \
   ${ZSS}/c/safIdtService.c \
+  ${ZSS}/c/eurekaClient.c \
   ${GSKLIB} ;
 then
   extattr +p ${ZSS}/bin/zssServer64
