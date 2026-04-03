e0939ec7c1c763a3274918f3c89fe5aa6384e827
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-04-03 13:17:41 -0400

    Optional flags for xlclang
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- build/build_zss.sh
+++ build/build_zss.sh
@@ -65,6 +65,7 @@ echo "Date stamp: $date_stamp"
 
 xlclang \
   -c \
+  ${ZWE_XLCLANG_FLAGS} \
   -qascii \
   "-Wc,ILP32,float(ieee),longname,langlvl(extc99),gonum,goff,ASM,asmlib('CEE.SCEEMAC','SYS1.MACLIB','SYS1.MODGEN')" \
   -DYAML_VERSION_MAJOR=${YAML_MAJOR} \
