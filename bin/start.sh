0e7b92c9362529900c1ba90bc627b8fa48556585
1000TurquoisePogs <sgrady@rocketsoftware.com>
2026-04-17 12:45:32 -0400

    tlstrace regardless of logging
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/start.sh
+++ bin/start.sh
@@ -173,7 +173,7 @@ if [[ "${OSNAME}" == "OS/390" ]]; then
     ZSS_SERVER="${ZSS_SERVER_31}"
   fi
 
-  if [ "$ZWE_components_zss_agent_https_trace" = "true" ] && [ "$ZWES_LOG_FILE" != "/dev/null" ]; then
+  if [ "$ZWE_components_zss_agent_https_trace" = "true" ]; then
     export GSK_TRACE_FILE="${ZWES_LOG_FILE}.tlstrace"
     export GSK_TRACE=0xFF
   fi
