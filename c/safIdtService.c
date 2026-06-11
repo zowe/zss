2eb0d6eae7bcf1d0daff1aa599f0fc5eef893325
Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>
2026-06-11 14:48:30 +0200

    Fix base64 padding length calculation
    Signed-off-by: Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>

--- c/safIdtService.c
+++ c/safIdtService.c
@@ -155,11 +155,11 @@ void extractUsernameFromJwt(HttpResponse *response, char *jwt, char *username) {
 
   // count on '=' padding characters that are omitted in JWT
   if (payLoadLength % 4 == 3) {
-    allocateLength = payLoadLength + 1;
+    allocateLength = payLoadLength + 2;  // count on '=' and '\0'
     base64Padding[0] = '=';
   }
   else if (payLoadLength % 4 == 2) {
-    allocateLength = payLoadLength + 2;
+    allocateLength = payLoadLength + 3;    // count on '==' and '\0'
     base64Padding[0] = '=';
     base64Padding[1] = '=';
   }
